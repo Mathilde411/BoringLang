93025348def6a3443b298f1e0ad571fab01d1697
Bastien Pascal <bastien.pascal@etu.imt-nord-europe.fr>
2023-07-28 15:20:38 +0200

    Added missing stack include

--- src/core/NamespacePath.cpp
+++ src/core/NamespacePath.cpp
@@ -15,6 +15,7 @@
 //     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <iostream>
+#include <stack>
 #include "boringlang/core/NamespacePath.hpp"
 
 using namespace BoringLang;
